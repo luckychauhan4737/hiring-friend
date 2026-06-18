354e5d921757406277a12da6684205d81d4a1e46
Lucky Chauhan <luckychauhan269@gmail.com>
2026-06-18 21:56:08 +0530

    Update blacklist.model.js

--- Backend/src/models/blacklist.model.js
+++ Backend/src/models/blacklist.model.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose")
 
 const blacklistTokenSchema = new mongoose.Schema({
     token: {
         type: String,
-        required: [ true, "token is required to be added in blacklist" ]
+        required: [true, "token is required to be added in blacklist"]
+    },
+    expiresAt: {
+        type: Date,
+        default: () => new Date(Date.now() + 24 * 60 * 60 * 1000),
+        expires: 0
     }
 }, {
     timestamps: true
 })
 
 const tokenBlacklistModel = mongoose.model("blacklistTokens", blacklistTokenSchema)
-
 
 module.exports = tokenBlacklistModel
